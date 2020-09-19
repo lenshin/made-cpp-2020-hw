ee571a62bea4289897c2a4d0fa471f21b84fbe31
lenshin <v.lenshin@gmail.com>
2020-09-19 17:05:33 +0300

    classify double

--- classify double/solution.c
+++ classify double/solution.c
@@ -15,7 +15,11 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    uint64_t one = 1;
+    uint64_t mask = one << index;
+    uint64_t temp = mask & number;
+    temp = temp >> index;
+    return temp;
 }
 
 
@@ -23,8 +27,31 @@ bool getBit (const uint64_t number, const uint8_t index) {
  * Checkers here:
  */
 
+bool checkBits (uint64_t number, uint8_t start, uint8_t stop, bool flag){
+    bool check = false;
+    for (uint8_t i = start; i <= stop; ++ i) {
+        if (getBit(number, i) == flag) {
+            check = true;
+            break;
+        }
+    }
+    return check;
+}
+
+bool checkNormal (uint64_t number) {
+    bool expContainZero = checkBits(number, 52, 62, false);
+    bool expContainOne = checkBits(number, 52, 62, true);
+    return expContainZero && expContainOne;
+}
+
+bool checkDenormal (uint64_t number) {
+    bool expAllZeros = !checkBits(number, 52, 62, true);
+    bool fracContainOne = checkBits(number, 0, 51, true);
+    return expAllZeros && fracContainOne;
+}
+
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0x0000000000000000;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,35 +59,48 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0x7FF0000000000000;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0xFFF0000000000000;
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    bool signPlus = checkBits(number, 63, 63, false);
+    bool normal = checkNormal(number);
+    return signPlus && normal;
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    bool signMinus = checkBits(number, 63, 63, true);
+    bool normal = checkNormal(number);
+    return signMinus && normal; 
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    bool signPlus = checkBits(number, 63, 63, false);
+    bool denormal = checkDenormal(number); 
+    return signPlus && denormal;
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    bool signMinus = checkBits(number, 63, 63, true);
+    bool denormal = checkDenormal(number);  
+    return signMinus && denormal;
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    bool expAllOne = !checkBits(number, 52, 62, false);
+    bool fracZero51 = checkBits(number, 51, 51, false);
+    bool fracContainOne = checkBits(number, 0, 50, true);
+    return  expAllOne && fracZero51 && fracContainOne;
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    bool expAllOne = !checkBits(number, 52, 62, false);
+    bool fracOne51 = checkBits(number, 51, 51, true);
+    return  expAllOne && fracOne51;
 }
 
 
@@ -108,4 +148,17 @@ void classify (double number) {
     else {
         printf("Error.\n");
     }
+}
+
+
+int main() {
+    double number;
+    scanf("%lf", &number);
+    for (uint8_t i = 0; i < 64; ++ i) {
+        printf("%d", getBit(convertToUint64(number), 63 - i));
+    }
+    printf("\n");
+    classify(number);
+
+    return 0;
 }
